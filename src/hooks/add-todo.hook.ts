7712f3610e7946fc01c3ea746da517bdfab27d85
Serhii Malyshko <malyshkosergiy@gmail.com>
2024-03-19 12:50:36 +0200

    soft fix

--- src/hooks/add-todo.hook.ts
+++ src/hooks/add-todo.hook.ts
@@ -5,14 +5,14 @@ import { useTodoSelectors } from '../selectors'
 import { AppDispatch, CreateTodoPayload } from '../types'
 import * as todoOperations from '../redux/todo/todo.operations'
 
-type UseAddTodoreturn = {
+type UseAddTodoReturn = {
   isFormOpen: boolean
   onAddTodo: () => void
   onSubmit: (values: CreateTodoPayload) => void
   onCloseForm: () => void
 }
 
-export const useAddTodo = ():UseAddTodoreturn => {
+export const useAddTodo = (): UseAddTodoReturn => {
   const [isFormOpen, setIsFormOpen] = React.useState(false)
 
   const dispatch: AppDispatch = useDispatch()
